65f5c665dcab226b18e64bb559bd63e462bfbc72
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2024-03-04 23:48:01 +0100

    add loading state to buffer data loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     //@ts-nocheck
+    import LoadingSpinner from "$lib/loader.gif";
     import Header from "./header.svelte";
     import Table from "./Table.svelte";
     import ComparisonTable from "./comparison_Table.svelte";
@@ -12,8 +13,11 @@
     import { get } from "svelte/store";
     import { onMount } from "svelte";
 
+    let loading = true;
+
     onMount(async () => {
         await load();
+        loading = false;
     });
 
     //TODO: implements themes in the rest of the app
@@ -24,16 +28,33 @@
     })
 </script>
 
-<Header styles = {styles}/>
-<PopUp/>
-{#if $comparing.is_comparing}
-<ComparisonTable styles = {styles}}/>
+{#if loading}
+    <div class="loading">
+        <img src={LoadingSpinner} alt="loading spinner">
+    </div>
 {:else}
-<Table styles = {styles}/>
+    <Header styles = {styles}/>
+    <PopUp/>
+    {#if $comparing.is_comparing}
+    <ComparisonTable styles = {styles}}/>
+    {:else}
+    <Table styles = {styles}/>
+    {/if}
+
+    {#if !$comparing.is_comparing}
+    <SavedStatus/>
+    {/if}
+    <LanguagePicker />
 {/if}
 
-{#if !$comparing.is_comparing}
-<SavedStatus/>
-{/if}
-<LanguagePicker />
+<style>
+    .loading {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
+
+
     
