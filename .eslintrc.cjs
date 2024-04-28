60046987ae8814123997b2b9cf8f9f8f77ffc357
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2024-04-28 23:44:31 +0200

    remove unused styles props

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,9 @@ module.exports = {
 		ecmaVersion: 2020,
 		extraFileExtensions: ['.svelte']
 	},
+	rules: {
+		"svelte/valid-compile": ["error", { "ignoreWarnings": true }]
+	},
 	env: {
 		browser: true,
 		es2017: true,
